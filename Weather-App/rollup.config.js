43e84d997fc1f455d9db2b02314a8acfad41dcf5
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-08-28 19:37:18 +0200

    Update rollup.config.js

--- Weather-App/rollup.config.js
+++ Weather-App/rollup.config.js
@@ -1,7 +1,12 @@
+import image from '@rollup/plugin-image';
+
 export default {
     input: 'src/index.js',
     output: {
       file: 'dist/bundle.js', 
       format: 'cjs',
     },
+    plugins: [
+        image(),
+      ],
   };
